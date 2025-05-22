cf506d1b0c7cabc8566d1ee101d0098bf42e1380
EnmaLlDev <enmanuel.lledo.1998@gmail.com>
2025-05-22 14:55:41 +0200

    Resuelto!

--- src/main/java/com/campusdual/classroom/Exercise13.java
+++ src/main/java/com/campusdual/classroom/Exercise13.java
@@ -5,7 +5,8 @@ public class Exercise13 {
         FuelTank ft = new FuelTank();
         ft.showDetails();
         System.out.println("Actualización capacidad");
-        ft.actualFuel = -8;
+        // linea inaccesible, el atributo ahora es privado y se usa getActualFuel()
+        //ft.actualFuel = -8;
         ft.showDetails();
     }
 }
